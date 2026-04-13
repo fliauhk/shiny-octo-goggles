6ef2a7341b5b1a12beab5178ef1f8719b180ed0b
fliauhk <rowanbean358@gmail.com>
2026-04-14 00:23:25 +0100

    server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/proxy', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Proxy server running at http://localhost:${PORT}`);
+    console.log(`Proxy server running on port ${PORT}`);
 });
